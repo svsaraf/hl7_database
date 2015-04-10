9a50520287ab64e864efb364f63aca8ad8967cc0
Sanjay <svsaraf90@gmail.com>
2015-04-09 23:04:06 -0400

    update

--- hl7/admin.py
+++ hl7/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from hl7.models import *
+
+admin.site.register(Document)
+admin.site.register(ComponentMSH)
+admin.site.register(ComponentEVN)
+admin.site.register(ComponentPID)
+admin.site.register(ComponentPD1)
+admin.site.register(ComponentNK1)
+admin.site.register(ComponentPV1)
+admin.site.register(ComponentIN1)
+
 # Register your models here.
