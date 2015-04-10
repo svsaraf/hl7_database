8b2bd583b124565e16d70f64a36fe07ca4d1f016
Sanjay <svsaraf90@gmail.com>
2015-04-09 23:24:38 -0400

    form posting

--- hl7/views.py
+++ hl7/views.py
@@ -4,8 +4,18 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 
+
 def index(request):
 	context = RequestContext(request)
+	if request.method=='POST':
+		text = request.POST['hl7doc']
+		doc = Document()
+		msh = ComponentMSH()
+		msh.doc = doc.docid
+		msh.text = text
+		doc.save()
+		msh.save()
+		return HttpResponseRedirect('/')
 	return render_to_response('index.html', {}, context)
 
 # Create your views here.
