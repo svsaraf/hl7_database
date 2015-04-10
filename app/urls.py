a089fb64a8b8d0c7cb920dcbc84cdb2ed2b74353
Sanjay <svsaraf90@gmail.com>
2015-04-10 01:38:48 -0400

    update

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'hl7.views.index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<num>[0-9]+)/$', 'hl7.views.recordview', name='recordview'),
 )
