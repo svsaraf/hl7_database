154e7b381012c9467a8bc024ee35a749a800d4af
Sanjay <svsaraf90@gmail.com>
2015-04-10 01:00:17 -0400

    update

--- hl7/views.py
+++ hl7/views.py
@@ -11,14 +11,14 @@ def index(request):
 		text = request.POST['hl7doc']
 		m = parse_message(text,2,True)
 		r = Record()
-		r.msh_9 = m.msh.msh_9
+		"""r.msh_9 = m.msh.msh_9
     	r.msh_10 = m.msh.msh_10
     	r.msh_11 = m.msh.msh_11
     	r.msh_12 = m.msh.msh_12
     	r.pid_5 = m.pid.pid_5
     	r.pid_11 = m.pid.pid_11
         r.evn_2 = m.evn.evn_2
-        r.evn_4 = m.evn.evn_4
+        r.evn_4 = m.evn.evn_4"""
         r.save()
         return HttpResponseRedirect('/')
 	return render_to_response('index.html', {}, context)
