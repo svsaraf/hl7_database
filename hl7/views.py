c1a0499f76f53104be8cb61b388cd0f665f6546d
Sanjay <svsaraf90@gmail.com>
2015-04-09 23:51:19 -0400

    update

--- hl7/views.py
+++ hl7/views.py
@@ -3,18 +3,27 @@ from hl7.models import *
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
-
+from hl7apy.parser import parse_message
 
 def index(request):
 	context = RequestContext(request)
 	if request.method=='POST':
 		text = request.POST['hl7doc']
+		message = parse_message(text)
 		doc = Document()
 		doc.save()
 		msh = ComponentMSH()
 		msh.doc = doc
-		msh.text = text
+		msh.text = message.msh
+		pid = ComponentPID()
+		pid.doc = doc
+		pid.text = message.pid
+		evn = ComponentEVN()
+		evn.doc = doc
+		evn.text = message.evn
 		msh.save()
+		pid.save()
+		evn.save()
 		return HttpResponseRedirect('/')
 	return render_to_response('index.html', {}, context)
 
