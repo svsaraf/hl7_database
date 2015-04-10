fb66a6326494088d1365711eb0b2739bcd342ea5
Sanjay <svsaraf90@gmail.com>
2015-04-09 23:16:34 -0400

    update

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'hl7.views.index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- hl7/views.py
+++ hl7/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from hl7.models import *
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+
+def index(request):
+	context = RequestContext(request)
+	return render_to_response('index.html', {}, context)
 
 # Create your views here.
